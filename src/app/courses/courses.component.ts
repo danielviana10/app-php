22f006c5a7b22f8bc7b7a830891bd172036f9956
danielviana10 <danielvianna43@gmail.com>
2025-02-28 20:03:44 -0300

    feat: adiciona funcionalidades de registro, atualização e exclusão de cursos com mensagens de sucesso e erro

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Course } from './courses';
 import { CoursesService } from './courses.service';
@@ -9,10 +8,11 @@ import { CoursesService } from './courses.service';
   styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
-
   courses: Course[] = [];
-
-  course!: new () => Course;
+  course: Course = new Course();
+  isLoading: boolean = false;
+  errorMessage: string = '';
+  successMessage: string = '';
 
   constructor(private courseService: CoursesService) {}
 
@@ -20,23 +20,93 @@ export class CoursesComponent implements OnInit {
     this.select();
   }
 
-  select(){
+  select() {
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
     this.courseService.getAllCourses().subscribe(
-      (response: Course[]) => {
-        this.courses = response;
+      (courses) => {
+        this.courses = courses;
+        this.isLoading = false;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.isLoading = false;
       }
-    )
+    );
   }
 
-  register(): void {
-    alert('Course registration successful for ');
+  register() {
+    if (this.course.id) {
+      this.errorMessage = 'Para atualizar um curso, use o botão "Update".';
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    this.courseService.registerCourse(this.course).subscribe(
+      (newCourse) => {
+        this.courses.push(newCourse);
+        this.course = new Course();
+        this.successMessage = 'Curso registrado com sucesso!';
+        this.isLoading = false;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.isLoading = false;
+      }
+    );
   }
 
-  update(): void {
-    alert('Course update succesfully for ');
+  update() {
+    if (!this.course.id) {
+      this.errorMessage = 'Selecione um curso para atualizar.';
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    this.courseService.updateCourse(this.course).subscribe(
+      (updatedCourse) => {
+        const index = this.courses.findIndex((c) => c.id === updatedCourse.id);
+        if (index !== -1) {
+          this.courses[index] = updatedCourse;
+        }
+        this.course = new Course();
+        this.successMessage = 'Curso atualizado com sucesso!';
+        this.isLoading = false;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.isLoading = false;
+      }
+    );
   }
 
-  delete(): void {
-    alert('Course deleted successfully for ');
+  delete(course: Course) {
+    this.isLoading = true;
+    this.errorMessage = '';
+    this.successMessage = '';
+
+    this.courseService.deleteCourse(course).subscribe(
+      () => {
+        this.courses = this.courses.filter((c) => c.id !== course.id);
+        this.successMessage = 'Curso deletado com sucesso!';
+        this.isLoading = false;
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.isLoading = false;
+      }
+    );
+  }
+
+  selectCourse(course: Course) {
+    this.course = { ...course };
   }
 }
--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Course } from './courses';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'; // Importação correta do map
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,15 +10,51 @@ import { map } from 'rxjs/operators'; // Importação correta do map
 export class CoursesService {
   url: string = 'http://localhost/api/php/';
 
-  courses: Course[] = [];
-
   constructor(private http: HttpClient) {}
 
   getAllCourses(): Observable<Course[]> {
     return this.http.get<{ courses: Course[] }>(this.url + 'list').pipe(
-      map((response) => {
-        this.courses = response.courses;
-        return this.courses;
+      map((response) => response.courses),
+      catchError((error) => {
+        return throwError(() => 'Erro ao carregar cursos: ' + error.message);
+      })
+    );
+  }
+
+  registerCourse(course: Course): Observable<Course> {
+    return this.http
+      .post<{ course: Course }>(this.url + 'register', course)
+      .pipe(
+        map((response) => response.course),
+        catchError((error) => {
+          return throwError(() => 'Erro ao registrar curso: ' + error.message);
+        })
+      );
+  }
+
+  updateCourse(course: Course): Observable<Course> {
+    if (course.id === undefined) {
+      return throwError(() => 'O curso precisa ter um ID para ser atualizado.');
+    }
+
+    return this.http.put<{ course: Course }>(this.url + 'update', course).pipe(
+      map((response) => response.course),
+      catchError((error) => {
+        return throwError(() => 'Erro ao atualizar curso: ' + error.message);
+      })
+    );
+  }
+
+  deleteCourse(course: Course): Observable<void> {
+    if (course.id === undefined) {
+      return throwError(() => 'O curso precisa ter um ID para ser deletado.');
+    }
+
+    const params = new HttpParams().set('id', course.id.toString());
+
+    return this.http.delete<void>(this.url + 'delete', { params }).pipe(
+      catchError((error) => {
+        return throwError(() => 'Erro ao deletar curso: ' + error.message);
       })
     );
   }
